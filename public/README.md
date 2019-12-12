c41cc471a7514530656e8c1e7a8d8dd7d61f0029
RajasG <rajasmgandhi@gmail.com>
2019-12-12 10:25:38 -0600

    Add files via upload

---
+++ public/README.md
@@ -0,0 +1,4 @@
+# nsgamezz
+---
+permalink: /index.html
+---
---
+++ public/formSubmit.php
@@ -0,0 +1,12 @@
+
+<!-- saved from url=(0048)http://www.weebly.com/weebly/apps/formSubmit.php -->
+<html><head> <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125558787-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-125558787-1');
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body></body></html>
